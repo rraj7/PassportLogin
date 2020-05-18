5903d5f9d5bc291be0fb5944f54fc14a6c018344
Rishi Raj <rraj7@uic.edu>
2020-05-18 02:14:05 -0500

    Added the passport-config file

--- server.js
+++ server.js
@@ -1,21 +1,44 @@
 var express = require('express');
 var app = express();
+var bcrypt = require('bcrypt');
+var passport = require('passport');
 
-app.set('view-engine','ejs')
+var initializePassport = require('./passport-config');
+initializePassport(passport);
+
+var users = []
+app.set('view-engine','ejs');
+app.use(express.urlencoded({extended: false}));
 
 app.get('/',(req,res)=> {
     res.render('index.ejs',{name : 'Rishi'})
 });
 
-app.get('/',(req,res)=> {
+app.get('/login',(req,res)=> {
     res.render('login.ejs'),{}
 });
 
-app.get('/',(req,res)=> {
+app.get('/register',(req,res)=> {
     res.render('register.ejs'),{}
 });
 
 app.post('/register',(req,res)=>{
+    try{
+
+        const hashedPassword = bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(), 
+            name: req.body.name, 
+            email: req.body.email, 
+            password: hashedPassword
+
+        })
+        res.redirect('/login')
+    }catch {
+
+        res.redirect('/register')
+    }
+    console.log(users) 
 
 })
 
